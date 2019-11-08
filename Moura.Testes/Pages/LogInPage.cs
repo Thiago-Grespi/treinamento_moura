08a1bc2d0e253864e3d6aa08d2b2b2a5470da9e9
Thiago Grespi Goulart <thiago.goulart@actminds.com>
2019-11-07 23:17:46 -0300

    First commit for real project

---
+++ Moura.Testes/Pages/LogInPage.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium;
+
+namespace Pages
+{
+
+    public class LogIn
+    {
+
+        private IWebDriver driver;
+
+        public string url = "https://mourasite1.jnmoura.com.br/PET_APRESENTACAO/Login.aspx";
+
+        public LogIn(IWebDriver driverAtual)
+        {
+            driver = driverAtual;
+        }
+
+        // Mapeamento de elementos
+        private IWebElement LogInInput() => driver.FindElement(By.Id("txtUsuario"));
+
+        private IWebElement SenhaInput() => driver.FindElement(By.Id("txtSenha"));
+
+        private IWebElement EntrarButton() => driver.FindElement(By.Id("btnEntrar"));
+
+        // Ação dos elementos
+        public void InserirLogin(string login)
+        {
+            LogInInput().SendKeys(login);
+        }
+
+        public void InserirSenha(string senha)
+        {
+            SenhaInput().SendKeys(senha);
+        }
+
+        public void ClicarEntrar()
+        {
+            EntrarButton().Click();
+        }
+
+        // Comportamentos
+        public void Logar(string login, string senha)
+        {
+            InserirLogin(login);
+            InserirSenha(senha);
+            ClicarEntrar();
+        }
+    }
+}
---
+++ Moura.Testes/Testes/LogInTestes.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Pages;
+using System.IO;
+using System.Reflection;
+
+namespace Testes
+{
+    [TestClass]
+    public class LogInTest
+    {
+        [TestMethod]
+        public void LogInComSucesso()
+        {
+            string chromeDriverPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            IWebDriver driver = new ChromeDriver(chromeDriverPath);
+            LogIn loginPage = new LogIn(driver);
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(loginPage.url);
+
+            loginPage.Logar("avalia", "111111");
+        }
+    }
+}
